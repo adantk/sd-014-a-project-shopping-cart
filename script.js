2e9c19d4ca680c4c25a6b3c7e3e51728ba61c469
Pablo Assunção <pabloassuncao2@gmail.com>
2021-08-12 20:30:15 -0300

    Adiciona do 1° ao 4° requisito

--- script.js
+++ script.js
@@ -12,32 +12,70 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// Requisito 3 - Remove o item clicado no carrinho
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  localStorage.removeItem(event.target.id);
+  event.target.remove();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
+  li.id = sku;
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-window.onload = () => { };
+// Requisito 2 - Função do Event Listener dos botões dos produtos que adiciona o produto clicado ao carinho
+async function productClickListener(event) {
+  console.log(event.target.parentElement);
+  const id = getSkuFromProductItem(event.target.parentElement);
+  const resRaw = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const resJson = await resRaw.json();
+  document.querySelector('.cart__items').appendChild(createCartItemElement(resJson));
+  localStorage.setItem(id, id);
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const btn = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  // Adiciona event listener nos produtos
+  btn.addEventListener('click', productClickListener);
+  section.appendChild(btn);
+  return section;
+}
+
+// Requisito 1 - Realiza a pesquisa e coloca na área de itens
+async function search(ele) {
+  const fetchRaw = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${ele}`);
+  const dataJson = await fetchRaw.json();
+  dataJson.results.forEach((product) => {
+    document.querySelector('.items').appendChild(createProductItemElement(product));
+  });
+}
+
+// Requisito 4 - Carrega o local Storage
+function loadStorage() {
+  for (let index = 0; index < localStorage.length; index += 1) {
+    const element = localStorage.getItem(localStorage.key(index));
+    fetch(`https://api.mercadolibre.com/items/${element}`)
+      .then((res) => res.json())
+      .then((e) => document.querySelector('.cart__items').appendChild(createCartItemElement(e)));
+    localStorage.setItem(element, element);
+  }
+}
+
+window.onload = () => {
+  search('computador');
+  loadStorage();
+};
