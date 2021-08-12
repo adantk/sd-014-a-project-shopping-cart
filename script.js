0f46d7639cfa09c194a88e2277693829edc8d608
Lucas Duarte <lucs.rkt@gmail.com>
2021-08-12 18:08:32 -0300

    requisito 1 finalzado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -19,7 +21,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 }
@@ -40,4 +44,35 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// Função assíncrona para trazer o array de produtos.
+// Primeiramente faço um fetch usando um endpoint,
+// depois faço o tratamento da promise, transformando-a
+// em um JSON. Por fim, retorno o array de produtos.
+const fetchProducts = async () => {
+  const request = await fetch(endpoint);
+  const response = await request.json();
+  const arrOfProducts = response.results;
+
+  return arrOfProducts;
+};
+
+/**
+ * Função que desestrutura o objeto passado por parâmetro
+ * e adiciona à section.items o elemento criado pela função
+ * createProductItemElement.
+ * @param {object} product Produto contido no arrOfProducts
+ */
+const createProducts = (product) => {
+  const { id: sku, title: name, thumbnail: image } = product;
+  document.querySelector('.items').appendChild(createProductItemElement({ sku, name, image }));
+};
+
+const addProducts = async () => {
+  const arrOfProducts = await fetchProducts();
+  arrOfProducts.forEach(createProducts);
+};
+
+window.onload = () => {
+  fetchProducts();
+  addProducts();
+};
