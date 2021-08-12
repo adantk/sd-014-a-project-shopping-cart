7746a6446976460ed90c4b57c5b753e6bb7e88db
Écio Ferraz <ecioferraz@hotmail.com>
2021-08-12 18:58:48 -0300

    lint

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const fetchProducts = async (query) => {
   const resp = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${query}`);
   const data = await resp.json();
   return data.results;
-}
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -20,11 +20,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({
-  id: sku,
-  title: name,
-  thumbnail: image
-}) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -36,29 +32,25 @@ function createProductItemElement({
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({
-  id: sku,
-  title: name,
-  thumbnail: salePrice
-}) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 window.onload = () => {
   fetchProducts('computador')
   .then((result) => result.forEach((pc) => {
     itemSection.appendChild(createProductItemElement(pc));
-  }))
+  }));
 };
