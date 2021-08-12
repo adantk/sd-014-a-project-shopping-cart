25da28fbd80569e3b49b15168dc09cae2edca5f6
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-08-12 14:24:25 -0300

    Iniciando

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createProductImageElement(imageSource) {
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
-  e.innerText = innerText;
+  e.innerText = innerText; 
   return e;
 }
 
