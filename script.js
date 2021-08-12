9a30afbfd7ac77c37242c40a56c1bdfedc1ddf3d
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-12 17:57:58 -0300

    Requisito 1 realizado

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 window.onload = () => { };
+
+const componenteHTML = document.querySelector('.items');
+fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+.then((response) => response.json())
+.then((response) => response.results.forEach((resultado) => {
+  componenteHTML.appendChild(createProductItemElement({
+    sku: resultado.id,
+    name: resultado.title,
+    image: resultado.thumbnail,
+  }));
+}));
+  // Código do requisito um baseado no trabalho do colega Fernando: https://github.com/tryber/sd-014-a-project-shopping-cart/pull/13/commits/114096fa1902021e1d50006347d430cb73d69e73
+  // 1 - Guardamos em uma variável a resultado do resgate do elemento com a classe 'items'.
+  // 2 - Buscamos a URL pedida no enunciado a partir da requisição fetch();
+  // 3 - Se a URL passada é válida, então transformamos o resultado da busca da URL em json;
+  // 4 - Se o json é gerado, então pegamos o atributo 'results' dentro do json e, para cada objeto contido dentro da array, teremos um elemento novo, resultado da função createProductItemElement(), dentro da section com classe 'items' com os atributos e valores id, título e imagem;
