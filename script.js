645d3d8cef03a5693539c62d759ff2dc8a6e650b
Semnome1505 <Gabriel_1505@outlook.com>
2021-08-12 16:28:47 -0300

    Adiciona o Requisito 2.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const secaoItens = document.querySelector('.items');
+const listaCompras = document.querySelector('.cart__items');
 
-function listaProdutos(url) {
+function retornoAPI(url) {
+  if (url === 'https://api.mercadolibre.com/sites/MLB/search?q=computador') {
+    return fetch(url)
+    .then((r) => r.json())
+    .then((r) => r.results);
+  }
   return fetch(url)
-  .then((r) => r.json())
-  .then((r) => r.results);
+  .then((r) => r.json());
 }
 
 function createProductImageElement(imageSource) {
@@ -13,10 +18,32 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  console.log(event);
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function pegaItem(evento) {
+  const id = evento.target.parentNode.firstChild.innerText;
+  const objeto = await retornoAPI(`https://api.mercadolibre.com/items/${id}`);
+  listaCompras.appendChild(createCartItemElement(objeto));
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.addEventListener('click', pegaItem);
+  }
   return e;
 }
 
@@ -33,7 +60,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 }
 
 async function insereNoDOM() {
-  const itens = await listaProdutos('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const itens = await retornoAPI('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   itens.forEach((item) => {
     const produto = createProductItemElement(item);
     secaoItens.appendChild(produto);
@@ -43,18 +70,6 @@ insereNoDOM();
 
 /*  function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 } */
 
 window.onload = () => { };
