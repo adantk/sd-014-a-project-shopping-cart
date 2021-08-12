e82cb29d6b339ecc6f99e60c20cb541c2ae0f638
Bruno Feu <brunofeu89@gmail.com>
2021-08-12 17:51:56 -0300

    add requirement 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource;
+  img.src = imageSource.results;
   return img;
 }
 
@@ -11,18 +11,42 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+// const {
+//   sku,
+//   name,
+//   image
+// } = product;
+
+
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+ 
   return section;
 }
+
+const getProduct = (search) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
+  .then((response) =>
+    response.json().then((searchResult) => {
+      searchResult.results.forEach((product) => {
+        const listItems = document.getElementsByClassName('items')[0];
+        listItems.appendChild(createProductItemElement({
+          sku: product.id,
+          name: product.title,
+          image: product.thumbnail,
+        }));
+      });
+    })
+  );
+};
+
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -32,7 +56,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,4 +64,11 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+
+const addProduct = async () => {
+await getProduct('computador');
+}
+
+window.onload = () => {
+  addProduct();
+};
