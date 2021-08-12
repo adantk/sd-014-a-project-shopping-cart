d197ef01fbf3e98d5b769bb3605be68ac88578d4
Eduardo Rocha <eduardorocha-dev@protonmail.ch>
2021-08-12 13:14:18 -0300

    Iniciando o projeto

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  //código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
