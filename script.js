0ad678a21a9cb77719268fae3fe4bfde72ab0e43
MartinSilva <martinempreendedor@gmail.com>
2021-08-12 18:31:11 -0300

    item 2

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText, sku) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  e.setAttribute('onclick', `apiRequ('${sku}')`);
   return e;
 }
 
-function createProductItemElement({
-  sku,
-  name,
-  image,
+function createProductItemElement({ // usado no item 1
+  id: sku,
+  title: name,
+  thumbnail: image,
 }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,15 +24,14 @@ function createProductItemElement({
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku));
   return section;
 }
-async function api(query) {
+async function api(query) { // item 1
   const arrayApi = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
     .then((mApi) => mApi.json())
     .then((mApiJson) => mApiJson.results);
-    
+
   arrayApi.forEach((item) => document.getElementsByClassName('items')[0]
     .appendChild(createProductItemElement(item)));
 }
@@ -41,19 +41,25 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+ // coloque seu código aqui
 }
 
-function createCartItemElement({
-  sku,
-  name,
-  salePrice,
+function createCartItemElement({ // usado no item 2
+  id: sku,
+  title: name,
+  price: salePrice,
 }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+async function apiRequ(id) { // item 1
+  await fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((mApi) => mApi.json())
+    .then((isso) => document.getElementsByClassName('cart__items')[0]
+      .appendChild(createCartItemElement(isso)));
 }
 
 window.onload = () => {
