8eecc367438f32bd230d049a9200da15de048147
tiagosathler <sathler@gmail.com>
2021-08-12 19:29:52 -0300

    Requisito 4: concluído

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const storageKey = 'ol-content';
+const cartItemsClass = '.cart__items';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -29,14 +32,16 @@ function createProductItemElement({ sku, name, image }) {
 // }
 
 function cartItemClickListener(event) {
-  event.target.parentNode.removeChild(event.target);
+  const cartItems = document.querySelector(cartItemsClass);
+  event.target.parentElement.removeChild(event.target);
+  localStorage.setItem(storageKey, cartItems.innerHTML);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);  
   return li;
 }
 
@@ -51,22 +56,29 @@ function getComputerItemFromMlApi(sku) {
   .then((response) => response.json());  
 }
 
-function addItemToCart(event) {
-  const cart = document.querySelector('.cart__items');
+function addItemToCart(event) {  
+  const cartItems = document.querySelector(cartItemsClass);
   const skuItem = event.target.parentElement.firstChild.innerText;
   getComputerItemFromMlApi(skuItem)  
   .then(({ id: sku, title: name, price: salePrice }) => {
     const li = createCartItemElement({ sku, name, salePrice });
-    cart.appendChild(li);
+    cartItems.appendChild(li);    
+    localStorage.setItem(storageKey, cartItems.innerHTML);
   });  
 }
 
-window.onload = () => {
+function getLocalStorage() {  
+  const cartItems = document.querySelector(cartItemsClass);
+  cartItems.innerHTML = localStorage.getItem(storageKey) ? localStorage.getItem(storageKey) : '';
+  cartItems.childNodes.forEach((li) => li.addEventListener('click', cartItemClickListener));  
+}
+
+window.onload = () => {  
   const items = document.querySelector('.items');
+  getLocalStorage();
   getComputersFromMlApi()
   .then((computers) => computers.forEach(({ id: sku, title: name, thumbnail: image }) => {
-    const section = createProductItemElement({ sku, name, image });
-    // console.log(section);
+    const section = createProductItemElement({ sku, name, image });    
     items.appendChild(section);
     items.lastChild.lastChild.addEventListener('click', addItemToCart);
   }));
