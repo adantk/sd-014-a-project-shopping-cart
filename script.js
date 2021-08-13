e948fb109a3a93b45446e51d8b5f01a0512227b1
danielknakano <81555352+danielknakano@users.noreply.github.com>
2021-08-12 22:14:37 -0300

    req 1

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -28,7 +28,7 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener() {
   // coloque seu código aqui
 }
 
@@ -40,4 +40,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+const addProductList = (products) => { // Adicionando cada item a section
+  products.forEach((product) => {
+    const element = createProductItemElement(product);
+    const sectionList = document.querySelector('.items');
+    sectionList.appendChild(element);
+  });
+};
+
+const fetchInit = async () => {
+  const getData = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computer');
+  const data = await getData.json();
+  data.results.forEach((result) =>
+    document.querySelector('.items').appendChild(createProductItemElement(result)));
+};
+
+window.onload = () => {
+  fetchInit();
+};
