81e8f9ccda5570ce60bbffa2729618b6f0349dea
Écio Ferraz <ecioferraz@hotmail.com>
2021-08-13 14:59:35 -0300

    req 2 e 3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const itemSection = document.querySelector('.items');
+const cartItems = document.querySelector('.cart__items');
 
+// O próprio VS Code sugeriu e transformou as funções em async
 const fetchProducts = async (query) => {
   const resp = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${query}`);
   const data = await resp.json();
@@ -36,21 +38,41 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  event.target.remove();
+}
 
-// function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// const fetchProduct = async (idSku) => {
+//   const resp = await fetch(`https://api.mercadolibre.com/items/${idSku}`);
+//   const data = await resp.json();
+//    createCartItemElement(data);
+// };
+// Havia feito o fetch separado, mas não consegui trabalhar com ele direito
+const addToCart = () => {
+  itemSection.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('item__add')) {
+      const idSku = event.target.parentElement.firstChild.innerText;
+      const resp = await fetch(`https://api.mercadolibre.com/items/${idSku}`);
+      const data = await resp.json();
+      return cartItems.appendChild(createCartItemElement(data));
+    }
+  });
+};
 
 window.onload = () => {
   fetchProducts('computador')
   .then((results) => results.forEach((result) => {
     itemSection.appendChild(createProductItemElement(result));
   }));
+  // fetchProduct();
+  addToCart();
 };
