53d3e1754f8be4a3263bff1ba72ad752cd049de3
Jonathan Ferreira <j0n4t@hotmail.com>
2021-08-13 15:56:44 -0300

    Requisito 4 ✔️

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ENDPOINTS = {
   item: 'https://api.mercadolibre.com/items/',
 };
 
+const CART = [];
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -35,6 +37,9 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   event.target.remove();
+  const productId = event.target.dataset.id;
+  CART.splice(CART.indexOf(productId), 1);
+  localStorage.setItem('CART', JSON.stringify(CART));
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -69,6 +74,7 @@ async function addProductToCart(productId) {
   const productInfo = await fetchProductInfo(productId);
   const cartItemElement = createCartItemElement(productInfo);
   cartItemElement.addEventListener('click', cartItemClickListener);
+  cartItemElement.dataset.id = productInfo.sku;
   document.querySelector('.cart__items').appendChild(cartItemElement);
 }
 
@@ -82,10 +88,23 @@ async function addProducts(section) {
   });
 }
 
+async function loadCart() {
+  CART.push(...JSON.parse(localStorage.getItem('CART') || '[]'));
+  if (CART.length > 0) {
+    CART.forEach(async (cartProductID) => {
+      await addProductToCart(cartProductID);
+    });
+  } else {
+    localStorage.setItem('CART', JSON.stringify(CART));
+  }
+}
+
 function clickAddProductToCartListener(event) {
   if (event.target.classList.contains('item__add')) {
     const productId = getSkuFromProductItem(event.target.parentElement);
     addProductToCart(productId);
+    CART.push(productId);
+    localStorage.setItem('CART', JSON.stringify(CART));
   }
 }
 
@@ -93,6 +112,7 @@ function clickAddProductToCartListener(event) {
 async function preparePage() {
   const productsSection = document.querySelector('.items');
   await addProducts(productsSection);
+  await loadCart();
   productsSection.addEventListener('click', clickAddProductToCartListener);
 }
 
