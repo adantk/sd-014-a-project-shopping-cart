dffaa4b96179435928e5194121dc965da27469b7
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-08-13 16:04:11 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({
+  sku,
+  name,
+  image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -32,7 +35,10 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({
+  sku,
+  name,
+  salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,20 +46,39 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const fetchMercadoLivre = (QUERY) => {
+const fetchMercadoLivre = async (QUERY) => {
   const mlApi = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
 
-  fetch(mlApi)
-  .then((response) => response.json())
-  .then((data) => data.results)
-  .then((results) => results.forEach((result) =>
-  document.querySelector('.items').appendChild(createProductItemElement({
-    sku: result.id,
-    name: result.title,
-    image: result.thumbnail,
-  }))));
+  await fetch(mlApi)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .then((results) => results.forEach((result) =>
+      document.querySelector('.items').appendChild(createProductItemElement({
+        sku: result.id,
+        name: result.title,
+        image: result.thumbnail,
+      }))));
 };
 
-window.onload = () => {
-  fetchMercadoLivre('computador');
+const fetchItem = () => {
+  const btn = document.querySelectorAll('.item__add'); // Busca a classe do botão no HTML
+
+  btn.forEach((list) => { // Cria um evento que para cada clique no botão seja adicionado o produto na lista do carrinho de compras!
+    list.addEventListener('click', async (event) => { 
+      const targett = event.target.parentElement.firstChild.innerHTML; 
+      const cart = document.querySelector('.cart__items'); // Busca a classe da lista do carrinho de compras!
+      const fetchItemMl = await fetch(`https://api.mercadolibre.com/items/${targett}`)
+        .then((response) => response.json()); // Transforma os dados em Json!
+      cart.appendChild(createCartItemElement({
+        sku: fetchItemMl.id,
+        name: fetchItemMl.title,
+        salePrice: fetchItemMl.price,
+      }));
+    });
+  });
+};
+
+window.onload = async () => {
+  await fetchMercadoLivre('computador');
+  await fetchItem();
 };
