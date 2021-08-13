700b51107236ee905deb97c86144d9f6cadd1793
Maurício Leite <mauricioleite1@gmail.com>
2021-08-12 22:25:15 -0300

    Requisito 02 e Novas alterações de estilo no SASS/CSS

--- script.js
+++ script.js
@@ -12,14 +12,40 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+function cartItemClickListener() {
+  
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+
+  return li;
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  const btn = section
+    .appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  btn.addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const salePrice = data.price;
+        const addedItem = createCartItemElement({ sku, name, salePrice });
+        document.querySelector('.cart__items').appendChild(addedItem);
+      });
+  });
 
   return section;
 }
@@ -34,24 +60,9 @@ fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
       const name = result.title;
       const image = result.thumbnail;
       const item = createProductItemElement({ sku, name, image });
+      
       document.querySelector('.items').appendChild(item);
     });
   });
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
 
 window.onload = () => { };
