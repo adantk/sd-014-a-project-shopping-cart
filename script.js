619dbf3fad434120a97c334901dc1f570c4f68dd
JosueTavaress <josueferreira8.jf@gmail.com>
2021-08-13 00:36:28 -0300

    add requisito 4

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function createProductItemElement({
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
 function cartItemClickListener(event) {
   event.target.remove();
@@ -51,33 +51,47 @@ function createCartItemElement({
 async function jsonComputer() {
   const js = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json());
-     js.results.forEach((element) => {
-       const elementos = document.querySelector('.items');
-      elementos.appendChild(createProductItemElement({ 
-         sku: element.id,
-         name: element.title,
-         image: element.thumbnail }));
-     });
-   }
+  js.results.forEach((element) => {
+    const elementos = document.querySelector('.items');
+    elementos.appendChild(createProductItemElement({
+      sku: element.id,
+      name: element.title,
+      image: element.thumbnail,
+    }));
+  });
+}
 
-   function addventListeBotton() {
-    const botton = document.querySelectorAll('.item__add');
-    botton.forEach((valu) => {
-      valu.addEventListener('click', async (event) => {
-        const x = event.target.parentElement.firstChild.innerText;
-        const carrinho = document.querySelector('.cart__items');
-        const produt = await fetch(`https://api.mercadolibre.com/items/${x}`)
+function addventListeBotton() {
+  const botton = document.querySelectorAll('.item__add');
+  botton.forEach((valu) => {
+    valu.addEventListener('click', async (event) => {
+      const x = event.target.parentElement.firstChild.innerText;
+      const carrinho = document.querySelector('.cart__items');
+      const produt = await fetch(`https://api.mercadolibre.com/items/${x}`)
         .then((y) => y.json());
-        carrinho.appendChild(createCartItemElement({
-          sku: produt.id,
-          name: produt.title,
-          salePrice: produt.price,
-        }));
-      });
+      carrinho.appendChild(createCartItemElement({
+        sku: produt.id,
+        name: produt.title,
+        salePrice: produt.price,
+      }));
+      localStorage.setItem('items', carrinho.innerHTML);
     });
-  }
+  });
+}
+
+ function carrinhoStorage() {
+  const items = localStorage.getItem('items');
+  const carrinho = document.querySelector('.cart__items');
+  carrinho.innerHTML = items;
+  console.log(items);
+  const li = document.querySelectorAll('.cart__item');
+  li.forEach((item) => {
+    item.addEventListener('click', cartItemClickListener);
+  });
+}
 
 window.onload = async () => {
   await jsonComputer();
   await addventListeBotton();
+  await carrinhoStorage();
 };
