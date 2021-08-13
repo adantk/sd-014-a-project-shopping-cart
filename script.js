820f4b170ddb58bf4c0d583aed74dc588194c738
Dogl4 <doougllas@hotmail.com.br>
2021-08-13 02:15:04 -0300

    c5 | 5° Requisito | Cria um objeto com funçoes para fazer calculos e outra funcao para atualizar no html o valor atual do total do carrinho.

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const ol = () => document.querySelector('.cart__items');
+const ol = document.querySelector('.cart__items');
+
+// Funcao que cria Elementos no html
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+// * Requisito 5
+// Calculadora
+const calculadora = {
+  add: (valor, total) => Math.round((valor + total) * 100) / 100,
+  sub: (valor, total) => Math.round((total - valor) * 100) / 100,
+};
+// Atualiza o preços
+const atualizaPrice = (callback, price) => {
+  let valorInit = 0;
+  const elementosOl = document.querySelectorAll('.cart__item');
+  const totalText = document.querySelector('.total-price');
+  elementosOl.forEach((elemento) => {
+    valorInit = calculadora.add(parseFloat(elemento.innerText.split('$')[1]), valorInit);
+  });
+  totalText.innerHTML = callback(price, valorInit);
+};
 
 //* Requisito 3
 function cartItemClickListener(e) {
+  atualizaPrice(calculadora.sub, (parseFloat(e.innerText.split('$')[1])));
   e.remove();
-  localStorage.setItem('carrinho', ol().innerHTML);
+  localStorage.setItem('carrinho', ol.innerHTML);
   // Referencia <https://developer.mozilla.org/en-US/docs/Web/API/Element/remove>
 }
 
 //* Requisito 4
 const recarregaCarrinho = () => {
-  ol().innerHTML = localStorage.getItem('carrinho');
-  ol().childNodes.forEach((el) => el.addEventListener('click', (e) =>
+  ol.innerHTML = localStorage.getItem('carrinho');
+  ol.childNodes.forEach((el) => el.addEventListener('click', (e) =>
     cartItemClickListener(e.target)));
+    atualizaPrice(calculadora.add, 0);
 };
 
 //* Requisito 2
@@ -25,12 +52,13 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 }
 // Acessa a API pelo Id
 const urlApiId = 'https://api.mercadolibre.com/items/';
-async function fetchApiId(event) {
-  return fetch(`${urlApiId}${event}`)
+async function fetchApiId(id) {
+  return fetch(`${urlApiId}${id}`)
   .then((response) => response.json())
   .then((dados) => {
-    ol().appendChild(createCartItemElement(dados)); // Adiciona no ol
-    localStorage.setItem('carrinho', ol().innerHTML);
+    ol.appendChild(createCartItemElement(dados)); // Adiciona no ol
+    localStorage.setItem('carrinho', ol.innerHTML); // Salva no localStorage
+    atualizaPrice(calculadora.add, 0);
   });
 }
 // Obtem o Id | Adiciona evento no botao, e retorna o id depois de clicado.
@@ -45,13 +73,6 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
-// Funcao que cria Elementos no html
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
 }
 // Cria section e adiciona class
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
