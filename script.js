f9dacf4818fb93dc04fd5c271dfe7cdea81e57bc
Lucas Rimis <lucasrimis@gmail.com>
2021-08-13 17:58:45 -0300

    Requisito 1 e tentando o requisito 2

--- script.js
+++ script.js
@@ -24,6 +24,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function fetchApiSection() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          data.results.map(((product) => document.querySelector('.items')
+          .appendChild(createProductItemElement({
+            sku: product.id,
+            name: product.title,
+            image: product.thumbnail,
+          }))));
+        });
+    });
+}
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -40,4 +55,31 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// item.forEach(() => button.addEventListener('click', () => {
+//   console.log('teste');
+// }));
+function fetchApiList(id) {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          document.querySelector('.cart__items')
+          .appendChild(createCartItemElement({
+            sku: data.id,
+            name: data.title,
+            salePrice: data.price,
+          }));
+        });
+    });
+}
+
+// document.querySelectorAll('.item__add').forEach((item) => {
+//   item.addEventListener('click', () => {
+//     const procuctId = document.querySelector('.item__sku').innerHTML;
+//     fetchApiList(procuctId);
+//   });
+// });
+
+window.onload = () => { 
+  fetchApiSection();
+};
