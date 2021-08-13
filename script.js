b555ddc9639ed994eb45670baaf3248005bdd65d
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-08-13 19:26:07 -0300

    fix lints e botao para limpar lista adicionado

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function createProductImageElement(imageSource) { //callback lista de itens
+function createProductImageElement(imageSource) { // callback lista de itens
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
-function createCustomElement(element, className, innerText) { //callback lista de itens
+function createCustomElement(element, className, innerText) { // callback lista de itens
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
@@ -15,7 +15,7 @@ function createCustomElement(element, className, innerText) { //callback lista d
 function createProductItemElement({ // cria lista de itens
   sku,
   name,
-  image
+  image,
 }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -34,13 +34,15 @@ function getSkuFromProductItem(item) { // retorna a SKU do pai
 
 function cartItemClickListener(event) { // função para apagar linha selecionada
   // coloque seu código aqui
-  event.target.remove()
+  // arr.splice(arr.indexOf(event.target.innerText.substring(5,18)),1)
+  // localStorage.list = JSON.stringify(arr)
+  event.target.remove();
 }
 
 function createCartItemElement({ // cria lista
   sku,
   name,
-  salePrice
+  salePrice,
 }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -48,31 +50,56 @@ function createCartItemElement({ // cria lista
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+let arr = [];
+let total = 0;
+const removeChild = () => {
+  const carIt = document.getElementsByClassName('cart__items');
+  while (carIt[0].firstChild) {
+    carIt[0].removeChild(document.getElementsByClassName('cart__items')[0].firstChild);
+  }
+};
+
+const listItem = (id) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`).then((resposta) => resposta.json())
+
+    .then((dados) => {
+      document.getElementsByClassName('cart__items')[0].appendChild(createCartItemElement({
+        sku: dados.id,
+        name: dados.title,
+        salePrice: dados.price,
+      }));
+      total += dados.price;
+      console.log(total);
+    });
+};
 
 const findId = (e) => {
-  listItem(getSkuFromProductItem(e.target.parentNode))
-}
-
+  listItem(getSkuFromProductItem(e.target.parentNode));
+  // arr.push(getSkuFromProductItem(e.target.parentNode));
+  // localStorage.setItem('list', JSON.stringify(arr));
+};
 const callFetch = () => {
-  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador").then((resposta) => {
-    return resposta.json()
-    })
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(url).then((resposta) => resposta.json())
+
     .then((dados) => {
-      dados.results.forEach((cur,i)=>{
-        document.getElementsByClassName('items')[0].appendChild(createProductItemElement({sku: cur.id,name: cur.title,image: cur.thumbnail}))
-        document.getElementsByClassName('item__add')[i].addEventListener('click',findId)
-      })
-  })
-}
-const listItem = (id) => {
-  fetch(`https://api.mercadolibre.com/items/${id}`).then((resposta) => {
-    return resposta.json()
-  })
-  .then((dados)=>{
-    document.getElementsByClassName('cart__items')[0].appendChild(createCartItemElement({sku:dados.id,name: dados.title,salePrice: dados.price}))
-  })
-}
-callFetch()
+      const contItem = document.getElementsByClassName('items');
+      const btnAdd = document.getElementsByClassName('item__add');
+      dados.results.forEach((cur, i) => {
+        contItem[0].appendChild(createProductItemElement({
+          sku: cur.id,
+          name: cur.title,
+          image: cur.thumbnail,
+        }));
+        btnAdd[i].addEventListener('click', findId);
+      });
+    });
+};
+
+callFetch();
 window.onload = () => {
-  
+  document.getElementsByClassName('empty-cart')[0].addEventListener('click', removeChild);
+  arr = localStorage.getItem('list');
+  arr = JSON.parse(arr);
+  // arr.forEach((cur)=> listItem(cur))
 };
