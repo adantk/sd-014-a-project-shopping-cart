94f55edff46b713bcaacc6070106fde38a03654d
Lucas Rimis <lucasrimis@gmail.com>
2021-08-14 18:43:45 -0300

    Requisito 6, mais conhecido como acalma coraçao :P

--- script.js
+++ script.js
@@ -62,14 +62,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
 function addProducts() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => {
@@ -85,31 +77,15 @@ function addProducts() {
     });
 }
 
-// function addToCart(id) {
-//   fetch(`https://api.mercadolibre.com/items/${id}`)
-//     .then((response) => {
-//       response.json()
-//         .then((data) => {
-//           document.querySelector('.cart__items').appendChild(createCartItemElement({
-//             sku: data.id,
-//             name: data.title,
-//             saleprice: data.price,
-//           }));
-//       });
-//     });
-// }
-
-// function teste() {
-// document.querySelectorAll('.item__add').forEach((btn) => {
-//   btn.addEventListener('click', () => {
-//   const btnParent = btn.parentElement;
-//   const productId = btnParent.firstChild.innerText;
-//   addToCart(productId);
-//   console.log(productId);
-// });
-// });
-// }
+function clearCart() {
+  const cart = document.querySelector('.cart__items');
+  const clearBtn = document.querySelector('.empty-cart');
+  clearBtn.addEventListener('click', () => {
+    cart.innerHTML = '';
+  });
+}
 
 window.onload = () => { 
   addProducts();
+  clearCart();
 };
