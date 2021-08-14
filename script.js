e92dea8be56795f1b7703d7a8f1bcc898b2e7a7d
Welton Lopes <weltonlopesmetal@gmail.com>
2021-08-14 12:54:14 -0300

    Resolvendo a questao 2

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-//Resolvendo a questão 1,
+
+// Questao 2. resolvida.....
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const criarBtn = () => {
+  const botao = document.querySelectorAll('.item__add');
+  botao.forEach((element) =>
+    element.addEventListener('click', async (event) => {
+      const carrinho = document.querySelector('.cart__items');
+      const produtos = event.target.parentElement;
+      const id = produtos.firstChild.innerText;
+
+      const productApi = await fetch(`https://api.mercadolibre.com/items/${id}`)
+      const retornoJson = await productApi.json();
+      carrinho.appendChild(createCartItemElement(retornoJson));
+    }));
+
+};
+
+//Resolvendo a questão 1.....
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const sectionItem = document.querySelector('.items');
   const section = document.createElement('section');
@@ -38,8 +63,9 @@ const transJson = async () => {
   arrayRates.forEach((product) => {
     createProductItemElement(product);
   });
+  criarBtn();
 };
-// ...........................
+// 
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -49,12 +75,6 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { transJson(); };
+window.onload = () => {
+  transJson();
+};
