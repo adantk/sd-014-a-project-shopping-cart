2631ac9c807dc1e0d4798e336f6e3b69cbd46dee
tiagosathler <sathler@gmail.com>
2021-08-14 16:07:15 -0300

    RASCUNHO: projeto todo refatorado com comentários para outras soluções. IMPORTANTE: mudança estrutural no HTML, invocando o 'script.js' ao final, possibilitando assim a declaração de constantes no ínicio do script

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const storageKey = 'ol-content';
-const cartItemsClass = '.cart__items';
-const emptyCartClass = '.empty-cart';
-const cartClass = '.cart';
+const cartSection = document.querySelector('.cart');
+const itemsSection = document.querySelector('.items');
+const cartItemsSection = document.querySelector('.cart__items');
+const emptyCartButton = document.querySelector('.empty-cart');
+const totalPriceSpan = document.querySelector('.total-price');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -18,112 +20,152 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
+  // function createProductItemElement(computer) {
+  // const { id: sku, title: name, thumbnail: image } = computer;
+  
+  // const sku = computer.id;
+  // const name = computer.title;
+  // const image = computer.thumbnail;
+
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-function sum(ol) {
-  const totalPrice = document.querySelector('.total-price');
-  const regex1 = /([$][1-9])\S+/i;
-  const regex2 = /([1-9])\S+/;
-  let total = 0;
-  ol.childNodes.forEach((li) => {
-    const found = li.innerText.match(regex1);
-    total += Number(found[0].match(regex2)[0]);
-  });
-  total = Math.round(100 * total) / 100;
-  totalPrice.innerText = total;
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
+function sum(ol) {
+    // const totalPrice = document.querySelector('.total-price');    
+    let total = 0;
+    ol.childNodes.forEach((li) => {
+      const found = li.querySelector('.sale-price-item').innerText;
+      total += Number(found);
+    });
+    total = Math.round(100 * total) / 100;
+    totalPriceSpan.innerText = total;
+  }
+
+// function sum(ol) { // ESTA SOLUÇÃO EXIGE OUTRO FORMATO PARA 'li.innerText'
+//   // const totalPrice = document.querySelector('.total-price');
+//   const regex1 = /([$][1-9])\S+/i;
+//   const regex2 = /([1-9])\S+/;
+//   let total = 0;
+//   ol.childNodes.forEach((li) => {
+//     const found = li.innerText.match(regex1);
+//     total += Number(found[0].match(regex2)[0]);
+//   });
+//   total = Math.round(100 * total) / 100;
+//   totalPriceSpan.innerText = total;
+// }
+
 function cartItemClickListener(event) {
-  const cartItems = document.querySelector(cartItemsClass);
-  event.target.parentElement.removeChild(event.target);
-  localStorage.setItem(storageKey, cartItems.innerHTML);
-  sum(cartItems);
+  // const cartItemsSection = document.querySelector(cartItemsSectionClass);
+  // console.log(event.target);  // pode ser a <span> da solução li.innerHTML
+  // console.log(event.currentTarget); // sempre será a pŕopria <li>
+  event.currentTarget.parentElement.removeChild(event.currentTarget);
+  localStorage.setItem(storageKey, cartItemsSection.innerHTML);
+  sum(cartItemsSection);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
+  const li = document.createElement('li');  
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $ ${salePrice}`;
+  const template = `SKU: ${sku} | NAME: ${name} | PRICE: $`;
+  const skuAndNameSpan = createCustomElement('span', 'sku-name-item', template);
+  // const skuAndNameSpan = createCustomElement('span', 'sku-name-item', `SKU: ${sku} | `);
+  // const nameSpan = createCustomElement('span', 'name-item', `NAME: ${name} | PRICE: $`);
+  const salePriceSpan = createCustomElement('span', 'sale-price-item', salePrice);  
+  li.appendChild(skuAndNameSpan);
+  // li.appendChild(skuSpan);
+  // li.appendChild(nameSpan);
+  li.appendChild(salePriceSpan);
   li.addEventListener('click', cartItemClickListener);
+  
   return li;
 }
 
 function createLoading() {
   const loadingSpan = document.createElement('span');
-  loadingSpan.innerText = 'Loading ...';
+  loadingSpan.innerText = 'Carregando página ...';
   loadingSpan.className = 'loading';
-  document.querySelector(cartClass).appendChild(loadingSpan);  
+  cartSection.appendChild(loadingSpan);  
 }
 
 function deleteLoading() {
-  document.querySelector(cartClass).removeChild(document.querySelector('.loading'));
+  cartSection.removeChild(document.querySelector('.loading'));
 }
 
-function getComputersFromMlApi() {
+async function getComputersFromMlApi() {
   createLoading();
   return fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
     .then((response) => response.json())
     .then((json) => json.results);
 }
 
-function getComputerItemFromMlApi(sku) {
+async function getComputerItemFromMlApi(sku) {
   createLoading();
   return fetch(`https://api.mercadolibre.com/items/${sku}`)
     .then((response) => response.json());    
 }
 
 function addItemToCart(event) {
-  const cartItems = document.querySelector(cartItemsClass);
-  const skuItem = event.target.parentElement.firstChild.innerText;
+  // const cartItemsSection = document.querySelector(cartItemsSectionClass);
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  // const skuItem = event.target.parentElement.firstChild.innerText; // Seria mais fácil...
+  const skuItem = getSkuFromProductItem(event.target.parentElement);
   getComputerItemFromMlApi(skuItem)
     .then(({ id: sku, title: name, price: salePrice }) => {
       const li = createCartItemElement({ sku, name, salePrice });
-      cartItems.appendChild(li);
-      localStorage.setItem(storageKey, cartItems.innerHTML);
-      sum(cartItems);
+      cartItemsSection.appendChild(li);
+      localStorage.setItem(storageKey, cartItemsSection.innerHTML);
+      sum(cartItemsSection);
     })
     .then(() => deleteLoading());
 }
 
 function getLocalStorage() {
-  const cartItems = document.querySelector(cartItemsClass);
-  cartItems.innerHTML = localStorage.getItem(storageKey)
+  // const cartItemsSection = document.querySelector(cartItemsSectionClass);
+  cartItemsSection.innerHTML = localStorage.getItem(storageKey)
     ? localStorage.getItem(storageKey)
     : '';
-  cartItems.childNodes.forEach((li) =>
+  cartItemsSection.childNodes.forEach((li) =>
     li.addEventListener('click', cartItemClickListener));
-  sum(cartItems);
+  sum(cartItemsSection);
 }
 
 function clearAll() {
-  const cartItems = document.querySelector(cartItemsClass);
-  cartItems.innerHTML = '';
-  sum(cartItems);  
+  // const cartItemsSection = document.querySelector(cartItemsSectionClass);
+  cartItemsSection.innerHTML = '';
+  // sum(cartItemsSection);  
+  totalPriceSpan.innerText = 0;
+  localStorage.removeItem(storageKey);
 }
 
 window.onload = () => {
-  const items = document.querySelector('.items');
-  document.querySelector(emptyCartClass).addEventListener('click', clearAll);
+  // const items = document.querySelector('.items');
+  // document.querySelector(emptyCartClass).addEventListener('click', clearAll);
+  emptyCartButton.addEventListener('click', clearAll);
   getLocalStorage();
   getComputersFromMlApi()
     .then((computers) =>
-      computers.forEach(({ id: sku, title: name, thumbnail: image }) => {
-        const section = createProductItemElement({ sku, name, image });
-        items.appendChild(section);
-        items.lastChild.lastChild.addEventListener('click', addItemToCart);      
+    computers.forEach((computer) => {
+        const { id: sku, title: name, thumbnail: image } = computer;        
+        // console.log({ sku, name, image });
+        const productItemSection = createProductItemElement({ sku, name, image });
+        // const section = createProductItemElement(computer);
+        // computers.forEach(({ id: sku, title: name, thumbnail: image }) => {
+        // const section = createProductItemElement({ sku, name, image });
+        productItemSection.lastChild.addEventListener('click', addItemToCart); // button
+        itemsSection.appendChild(productItemSection);
+        // items.lastChild.lastChild.addEventListener('click', addItemToCart);
       }))
     .then(() => deleteLoading());
 };
