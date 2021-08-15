d8eb16ef1218cd3644ea7adfce50fcd96e965fc5
VSSSP <victorssspaula@live.com>
2021-08-14 22:06:40 -0300

    Requisito 2 incompleto

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const items = document.querySelector('.items');
+let cart;
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -15,6 +16,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  // Restringe o results para apenas as chaves id, title e thumbnail
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,7 +36,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -42,12 +44,38 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+// Requisito 1
 async function getComputer(query) {
-  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-  const json = await api.json();
-  json.results.map((result) => items.appendChild(createProductItemElement(result)));
+  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`); // Busca api
+  const json = await api.json(); // Deixa a API 'legivel' pro JS com o Json, transformando ela em objeto
+  json.results.forEach((result) => items.appendChild(createProductItemElement(result)));
+  // mapeia os results e coloca como parametro de createProductItemElement deixando ela como filha de items
 }
 
-window.onload = () => { 
+// Requisito 2
+async function getCart(itemId) {
+  const api = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const json = await api.json();
+  cart = {
+    id: json.id,
+    title: json.title,
+    price: json.price,
+  };
+  const addCart = document.querySelector('.cart__items');
+  return addCart.appendChild(createCartItemElement(cart));
+}
+
+function getId() {
+  const button = document.getElementsByClassName('item__add');
+  button.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const id = getSkuFromProductItem(event.target.parentNode);
+      getCart(id);
+    });
+  });
+}
+
+window.onload = () => {
   getComputer('computador');
+  getId();
 };
