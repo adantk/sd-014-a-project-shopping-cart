c58e9800c8ab82bff69c0d4ee13b17cb00d9d871
VSSSP <victorssspaula@live.com>
2021-08-15 17:08:59 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -15,23 +15,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  // Restringe o results para apenas as chaves id, title e thumbnail
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -41,10 +24,46 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  return li;
+  return li; // retorna uma li com o id, nome e preço do produto clicado
 }
 
-// Requisito 1
+async function getCart(itemId) {
+  const api = await fetch(`https://api.mercadolibre.com/items/${itemId}`); // retorna a api do produto encontrado pela id
+  const json = await api.json();
+  cart = {
+    id: json.id,
+    title: json.title,
+    price: json.price,
+  }; // Objeto para eu trabalhar apenas com o que eu preciso da api do produto
+  const addCart = document.querySelector('.cart__items');
+  return addCart.appendChild(createCartItemElement(cart)); // chama como filho o retorno da createCart(...) entregando meu objeto cart como parametro
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText; // Pega o id do span/item__sku dentro daquela section clicada
+}
+
+function getId(event) {
+  const id = getSkuFromProductItem(event.target.parentNode); 
+  // Pega o pai do elemento button clicado e passa como parametro para getSku(...)
+  getCart(id); // chama getCart com o id encontrado
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  // Restringe o results para apenas as chaves id, title e thumbnail
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.appendChild(button);
+  button.addEventListener('click', getId); // Adiciona eventListener nos botões criados e chama getId
+
+  return section;
+}
+
 async function getComputer(query) {
   const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`); // Busca api
   const json = await api.json(); // Deixa a API 'legivel' pro JS com o Json, transformando ela em objeto
@@ -52,30 +71,6 @@ async function getComputer(query) {
   // mapeia os results e coloca como parametro de createProductItemElement deixando ela como filha de items
 }
 
-// Requisito 2
-async function getCart(itemId) {
-  const api = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
-  const json = await api.json();
-  cart = {
-    id: json.id,
-    title: json.title,
-    price: json.price,
-  };
-  const addCart = document.querySelector('.cart__items');
-  return addCart.appendChild(createCartItemElement(cart));
-}
-
-function getId() {
-  const button = document.getElementsByClassName('item__add');
-  button.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
-      const id = getSkuFromProductItem(event.target.parentNode);
-      getCart(id);
-    });
-  });
-}
-
 window.onload = () => {
   getComputer('computador');
-  getId();
 };
