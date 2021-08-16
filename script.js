511ef2279be526f350652b8db6855cde5e3a3c94
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-08-16 16:54:44 -0300

    requisito 2 - pegando o item e add ao carrinho

--- script.js
+++ script.js
@@ -20,7 +20,13 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
  const btnAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  btnAdd.addEventListener('click', addCarrinho);
+ // btnAdd.addEventListener('click', addCarrinho);
+  btnAdd.addEventListener('click', (event) => {
+    console.log(event.target);
+    console.log(sku);
+    fetchItens(sku);
+  });
+  
   section.appendChild(btnAdd);
 
   return section;
@@ -31,11 +37,13 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
+ //  itemCarrinho.removeChild(event.target); // Pega o item que foi clicado e remove ele pelo evento .target
+
   // pegando a lista de elementos do carrinho
-  const listaCarrinho = document.querySelectorAll('li.cart__item');
-  listaCarrinho.addEventListener('click', function (evento) {
-    console.log(evento.target); // pega o evento, qual o item clicado e remove ele da lista
-  });
+  // const listaCarrinho = document.querySelectorAll('li.cart__item');
+  // listaCarrinho.addEventListener('click', function (evento) {
+  //   console.log(evento.target); // pega o evento, qual o item clicado e remove ele da lista
+  // });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -51,7 +59,7 @@ const fetchApi = async () => {
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
   const response = await fetch(endpoint); // retorna uma promise
   const responseJson = await response.json(); // json retorna uma promise
-  return responseJson;
+  return responseJson.results;
     /* Estudo realizado junto com o Gustavo Dias - forma de explicar direfente de como verificar se a Promise esta tendo o retorno desejado. */
     // if (response.ok) { 
     //   const jsonResponse = await response.json();
@@ -64,7 +72,7 @@ const getProduto = async () => {
   const item = document.querySelector('.items'); 
   // pegando a classe e criando nossos elementos (produtos) com o forEach de baixo
 
-  produtos.results.forEach((resul) => {
+  produtos.forEach((resul) => {
     item.appendChild(createProductItemElement(
       { 
         sku: resul.id, 
@@ -78,28 +86,31 @@ const getProduto = async () => {
 
 // 2
 const fetchItens = async (itemId) => {
+  console.log(itemId);
   const fetchItem = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
   const responseItem = await fetchItem.json();
-  // console.log(responseItem);
+  console.log(responseItem);
+  addCarrinho(responseItem);
+  // const { id, automatic_relist, available_quantity } = responseItem;
+  // console.log(id, automatic_relist, available_quantity);
   return responseItem;
 };
 
 function addCarrinho(idItem) {
   console.log(idItem);
-  fetchItens(idItem).then((item) => {
+  
   const itemId = createCartItemElement( // envia os parametros do id do item recebido para a função createCartItemElement
-    { sku: item.id, 
-      name: item.title, 
-      salePrice: item.price },
-
-      )
-      const itemCarrinho = document.querySelector('.cart__items');
-      itemCarrinho.appendChild(itemId);
-    });
-     
+    { sku: idItem.id, 
+      name: idItem.title, 
+      salePrice: idItem.price,
+    },
+  )
+    const itemCarrinho = document.querySelector('.cart__items');
+    itemCarrinho.appendChild(itemId);     
 }
 
-window.onload = () => { 
-  fetchApi();
+window.onload = async () => { 
+  await fetchApi();
+
   getProduto();
 };
