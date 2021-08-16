52c82ebeb898da2adee40e3f21b3b4d166e607d6
Flavio Pires <flavioasp77@gmail.com>
2021-08-16 16:33:12 -0300

    Requisito 1 finalizado

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -21,7 +21,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+ 
   return section;
 }
 
@@ -41,4 +41,22 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// inicio do projeto
+// função assincrona para conectar com a API e pegar os resultados desejados
+const getResultsFromAPI = async (search) => {
+  const itemsAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+  // pega uma chave específica.
+  const responseItemsAPI = await itemsAPI.json().then((e) => e.results);
+  const sectionItens = document.querySelector('.items');
+  // passar por cada elemento retonado do responseItemsAPI
+  responseItemsAPI.forEach((element) => {
+    // guardando o retono do forEach e chamando a função para criar a estrutura dos itens na pagina
+    const returnEle = createProductItemElement(element);
+    // criando dimanicamente os elementos na página dentro da section items
+    sectionItens.appendChild(returnEle);
+  });
+};
+
+window.onload = () => { 
+  getResultsFromAPI('computador');
+};
