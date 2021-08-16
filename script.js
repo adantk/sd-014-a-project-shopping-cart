c4c86a37f08703477f2089d53da4f4b355113569
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-08-16 10:18:39 -0300

    Requisito 01

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const items = document.querySelector('.items');
+// const buttom = document.querySelector('.item__add');
+
+// Dadas Funções para o projeto:
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -24,20 +28,42 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+// Preparation:
+const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+const type = 'computer';
+
+// Requisito 01:
+const itemsToHTML = async () => {
+  const url = await fetch(URL + type);
+  const urlData = await url.json();
+  const { results } = urlData;
+
+  results.forEach((data) => {
+    const c = createProductItemElement({ sku: data.id, name: data.title, image: data.thumbnail });
+    items.appendChild(c);
+  });
+};
+
+// -------------------------------------------------------------
+
+// window.onload = () => { itemsToHTML(); };
+
+window.onload = async () => {
+  await itemsToHTML(); 
+};
