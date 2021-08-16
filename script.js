4e23a51b3d5993272c35efb3d8c1fbb34d472d08
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-08-16 14:25:44 -0300

    requisito 2 refatorado

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// const saveCarrinho = () => {
+//   localStorage.clear();
+//   localStorage.setItem('ok', meuCarrinho.innerHTML);
+// };
+
+// const loadCarrinho = () => {
+//   meuCarrinho.innerHTML = localStorage.getItem('ok');
+// };
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -30,9 +39,10 @@ function getSkuFromProductItem(item) {
 // 3
 function cartItemClickListener(event) {
   event.target.remove();
+//   // saveCarrinho();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -48,36 +58,65 @@ function achaProduto(params) {
 
 // consegue informação da API 
 const criaProduto = async (query) => {
+  const loading = document.getElementById('loading');
+  loading.innerText = 'loading...';
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
   const requesicao = await fetch(endpoint);
   const resposta = await requesicao.json();
-
+  loading.remove();
+  
   achaProduto(resposta.results);
 };
+
+const carrinho = document.querySelector('.cart__items');
+
 // 2
-const cartItem = async () => {
-  const botao = document.querySelectorAll('.item__add');
-  const carrinho = document.querySelector('.cart__items');
-  // Adiciona um listener para cada botao da minha lista de itens
-  botao.forEach((botaoCli) => { 
-    botaoCli.addEventListener('click', async (event) => {
-      const itemId = getSkuFromProductItem(event.target.parentElement);
-      const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
-      const requesicao = await fetch(endpoint);
-      const response = await requesicao.json();
-      // Adiciona informações do produto ao carrinho 
-      const itens = { 
-        sku: response.id,
-        name: response.title,
-        salePrice: response.price };
-      carrinho.appendChild(createCartItemElement(itens));
-    });
-  });
+// funçao para adicionar o produto
+function adicionaAoCarrinho(params) {
+  document.querySelector('.cart__items').appendChild(createCartItemElement(params));
+}
+
+// função para pegar informaçoes do item clicado
+const infoItem = async ({ target }) => {
+  const itemId = getSkuFromProductItem(target.parentNode);
+  const endpoint = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const response = await endpoint.json();
+  
+  adicionaAoCarrinho(response);
 };
+
+// função para o evento 'click' adicionar o item no carrinho
+function botaoAdiciona() {
+  const botao = document.querySelectorAll('.item__add');
+  botao.forEach((add) => {
+    add.addEventListener('click', infoItem);
+  });
+}
+
+// 2
+// const cartItem = async () => {
+//   const botao = document.querySelectorAll('.item__add');
+//   const carrinho = document.querySelector('.cart__items');
+//   // Adiciona um listener para cada botao da minha lista de itens
+//   botao.forEach((botaoCli) => { 
+//     botaoCli.addEventListener('click', async (event) => {
+//       const itemId = getSkuFromProductItem(event.target.parentElement);
+//       const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+//       const requesicao = await fetch(endpoint);
+//       const response = await requesicao.json();
+//       // Adiciona informações do produto ao carrinho 
+//       const itens = { 
+//         sku: response.id,
+//         name: response.title,
+//         salePrice: response.price };
+//       carrinho.appendChild(createCartItemElement(itens));
+//     });
+//   });
+// };
 
 // 6 limpa carrinho
 
 window.onload = async () => { 
   await criaProduto('computador');
-  cartItem();
+  botaoAdiciona();
 };
