bd86fc62f067d6353432ed4352a6f3f2dc4fd9b6
Augusto <augusto.raminelli>
2021-08-16 23:13:40 +0200

    requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -16,6 +17,7 @@ function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
+  section.className = 'item';
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -23,36 +25,71 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
+const clearProductList = () => {
+  document.getElementsByClassName('items')[0].innerText = ''; // a função remove todos os itens da lista substituidno a lista por uma string vazia.
+  console.log('learProductList', document.getElementsByClassName('items'));
+};
+const showProductList = async () => {
+  clearProductList();
+  const responseRaw = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  const responseJson = await responseRaw.json();
+
+  responseJson.results.forEach((element) => {
+    const itemEl = createProductItemElement(
+      {
+        sku: element.id, name: element.title, image: element.thumbnail,
+      },
+    );
+    document.getElementsByClassName('items')[0].appendChild(itemEl);
+  });
+  buttonAddCart();
+};
+
+const clearCartList = () => {
+  document.getElementsByClassName('cart__items')[0].innerText = '';
+};
+
+const showCartList = () => {
+  clearCartList();
+  const cart = document.querySelector('.cart__items');
+  cartList.forEach((productJson) => {
+    cart.appendChild(createCartItemElement(productJson));
+  });
+};
+let cartList = [];
+const buttonAddCart = () => {
+  const button = document.querySelectorAll('.item__add');
+  button.forEach((item) => item.addEventListener('click', async (event) => {
+    const selectedProduct = event.target.parentElement;
+    const sku = selectedProduct.firstChild.innerText;
+
+    const productJson = await fetch(
+      `https://api.mercadolibre.com/items/${sku}`,
+    ).then((r) => r.json());
+    cartList.push(productJson);
+    // console.log(cartList);
+    showCartList();
+  }));
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 // requisito 1 grupo de estudo com Alvaro Raminelli dev Pleno CoinBase
-window.onload = async () => {
-    // const chartList = []; 
-    // chartList.forEach((item) => {
-    //   const itemEl = cre
-    // });
-    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((res) => res.json());
-    response.results.forEach((element) => {
-    const itemEl = createProductItemElement(
-      { 
-        sku: element.id, name: element.title, image: element.thumbnail,
-        },
-      );
-    document.getElementsByClassName('items')[0].appendChild(itemEl);
-  });
+window.onload = () => {
+  showProductList();
+  showCartList();
 };
