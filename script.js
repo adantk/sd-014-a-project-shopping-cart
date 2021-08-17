97c8cd18b82750370aa83076b20df5d2cfa5194d
Matheus Souza <ms.conrado25@hotmail.com>
2021-08-17 12:31:27 -0300

    requisito 4 - adicionando e removendo os itens do localStoragae

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const valorfinal = document.querySelector('.total-price');
 const botaoEsvaziar = document.querySelector('.empty-cart');
 const h1 = document.createElement('h1');
 const body = document.querySelector('body');
-let soma = 0;
+let soma = Number(localStorage.getItem('valor'));
 valorfinal.innerHTML = 0;
 
-// função de click para esvaziar o carrinho e resetar o preço.
+// função de click para esvaziar o carrinho, resetar o preço e apagar tudo do local storage.
 botaoEsvaziar.addEventListener('click', () => {
   cartItens.innerHTML = '';
   valorfinal.innerHTML = 0;
   soma = 0;
+  localStorage.clear();
 });
 
 // função de loanding, ele roda enqunato os produtos da API não carregam e é apagado apos os produtos aparecer.
@@ -38,12 +39,10 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
@@ -51,14 +50,30 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// função implementada para remover o item da lista clicando encima dele.
+// adicionadno os items do carrinho ao localStorage
+function addStorage() {
+  localStorage.setItem('lista', cartItens.innerHTML);
+}
+
+// função implementada para remover o item da lista clicando encima dele e fazendo a subtração do valor, salvando novamente o localStorage com sem o item removido
 function cartItemClickListener(event) {
   const evento = event.target;
   cartItens.removeChild(evento);
-  const valorString = evento.innerHTML.split('$');
-  const valorNumber = parseFloat(valorString[1]);
+  const valorString = evento.innerHTML.split('$')[1];
+  const valorNumber = parseFloat(valorString);
   soma -= valorNumber;
-  valorfinal.innerText = Math.round(soma * 100) / 100;
+  const resultadoValor = Math.round(soma * 100) / 100;
+  valorfinal.innerText = resultadoValor;
+  localStorage.setItem('valor', resultadoValor);
+  addStorage();
+}
+
+// função chamada para conseguir remover o item da lista despois de recarregar a pagina.
+function removerItemLocal() {
+  const li = document.querySelectorAll('.cart__item');
+  li.forEach((lista) => {
+    lista.addEventListener('click', cartItemClickListener);
+  });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -72,17 +87,20 @@ function createCartItemElement({ sku, name, salePrice }) {
 // função para fazer a soma dos produtos que são adiconados ao carrinho.
 function somandoValorCarrinho(valor) {
   soma += valor;
-  valorfinal.innerText = Math.round(soma * 100) / 100;
+  const valorFinal = Math.round(soma * 100) / 100;
+  valorfinal.innerText = valorFinal;
+  localStorage.setItem('valor', valorFinal);
 }
 
-// função para adicionar os produtos no carrinho de compras!
+// função para adicionar os produtos no carrinho de compras e chamando função para adicionar os itens no localStorage.
 function adiconandoALista(idSKU) {
   fetch(`https://api.mercadolibre.com/items/${idSKU}`)
     .then((itemSelecionado) => itemSelecionado.json().then((pc) => {
       const objetoPc = { sku: pc.id, name: pc.title, salePrice: pc.price };
       cartItens.appendChild(createCartItemElement(objetoPc));
       somandoValorCarrinho(pc.price);
-    }));
+    }))
+    .then(() => addStorage());
 }
 
 // função de click para pegar o id do produto e adicionar ele na lista de compras.
@@ -91,8 +109,19 @@ function pegandoIdProduto() {
   buttom.forEach((botaoAdd) => {
     botaoAdd.addEventListener('click', (event) => {
       const evento = event.target;
-      adiconandoALista(getSkuFromProductItem(evento.parentNode));
+      adiconandoALista(getSkuFromProductItem(evento.parentElement));
     });
+  });
+}
+
+// função para adicionar na pagina os itens salvos do localStorage depois de recarregar.
+function loadingStorage() {
+  Object.keys(localStorage).forEach((key, index) => {
+    if (localStorage.key(index) === 'valor') {
+      valorfinal.innerHTML = localStorage.getItem('valor');
+    } else {
+      cartItens.innerHTML += localStorage.getItem(key);
+    }
   });
 }
 
@@ -106,7 +135,9 @@ const mercadoLivre = () => {
           sectionItens.appendChild(createProductItemElement(objetoPc));
         });
       }).then(() => pegandoIdProduto())
-        .then(() => body.removeChild(h1));
+        .then(() => body.removeChild(h1))
+        .then(() => loadingStorage())
+        .then(() => removerItemLocal());
     });
 };
 
