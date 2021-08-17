122d1c9db2622b971bbe28944124181bac049d64
Thiago Oshiro <oshirothiago@gmail.com>
2021-08-16 22:21:23 -0300

    alteraçoes lint

--- script.js
+++ script.js
@@ -15,22 +15,23 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  const itemPai = document.querySelector('.items');
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  const itemPai = document.querySelector('.items')
   itemPai.appendChild(section);
   return section;
 }
 
-const prodFetch = () => { fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-.then((response) => response.json())
-  .then((itemCarr) => itemCarr.results.forEach(({id: sku, title: name, thumbnail: image}) => {
-    createProductItemElement({sku, name, image});
-  }))
-}
+//  função requisito 1;
+const prodFetch = () => { 
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((itemCarr) => itemCarr.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    createProductItemElement({ sku, name, image });
+  }));
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -41,21 +42,21 @@ function cartItemClickListener(event) {
   
 }
 
+//  funções requisito 2;
+const fetchId = async (id) => {
+  const pegarIdRaw = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const IdTrat = await pegarIdRaw.json();
+  return console.log(IdTrat);
+};
+
 const lerBotao = () => {
-const pegaBotao = document.querySelectorAll('.item__add');
-pegaBotao.forEach((button) => button.addEventListener('click', clickBotao))
-}
-
-const clickBotao = (evento) => {
-  fetchId(evento.target.parentElement.firstChild.innerText);
-  console.log('funciona');
-}
-
-const fetchId =  async (id) => {
-const pegarIdRaw = await fetch(`https://api.mercadolibre.com/items/${id}`)
-const IdTrat = await pegarIdRaw.json();
-console.log(IdTrat);
-}
+console.log('rodando');
+const listaItems = document.querySelectorAll('.item__add');
+console.log(listaItems);
+listaItems.forEach((button) => button.addEventListener('click', async (event) => {
+ await fetchId(event.target.parentElement.firstChild.innerText);
+}));
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
