d9236db607972e5f95165443cc91b62e7f831a5e
Augusto <augusto.raminelli>
2021-08-18 14:50:22 +0200

    ajustes lint

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cartList = [];
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,37 +28,34 @@ function createProductItemElement({ sku, name, image }) {
 }
 const clearProductList = () => {
   document.getElementsByClassName('items')[0].innerText = ''; // a função remove todos os itens da lista substituidno a lista por uma string vazia.
-  console.log('learProductList', document.getElementsByClassName('items'));
+  console.log('clearProductList', document.getElementsByClassName('items'));
 };
 
 function showLoading() {
   document.getElementsByClassName('items')[0]
-  .appendChild(createCustomElement('span', 'loading', 'Loading'));
+    .appendChild(createCustomElement('span', 'loading', 'Loading'));
 }
 
 function hideLoading() {
   document.getElementsByClassName('items')[0].innerText = '';
 }
-const showProductList = async () => {
-  clearProductList();
-  showLoading(); 
-  const responseRaw = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const responseJson = await responseRaw.json();
-  hideLoading();
-  responseJson.results.forEach((element) => {
-    const itemEl = createProductItemElement(
-      {
-        sku: element.id, name: element.title, image: element.thumbnail,
-      },
-    );
-    document.getElementsByClassName('items')[0].appendChild(itemEl);
-  });
-  buttonAddCart();
-};
 
 const clearCartList = () => {
   document.getElementsByClassName('cart__items')[0].innerText = '';
 };
+
+function showCartTotal() {
+  const total = cartList.reduce((acc, item) => acc + item.price, 0);
+  document.getElementsByClassName('total-price')[0].innerText = total;
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.id = sku; // vai passar esse ID para a função de click para filtrar do array
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  return li;
+}
 
 const showCartList = () => { // função que busca a informação e demonstra a lista do carrinho 
   clearCartList(); // função que limpa o carrinho antes de fazer a novamente a busca no cart__items
@@ -66,7 +65,15 @@ const showCartList = () => { // função que busca a informação e demonstra a 
   });
   showCartTotal();
 };
-let cartList = [];
+
+function cartItemClickListener(event) { // Função que remove o item do carrinho quando clicado
+  if (event.target.classList.contains('cart__item')) {
+    cartList = cartList.filter((item) => item.id !== event.target.id);
+    showCartList();
+  } // console.log(event.target.id);
+  // filter está retirando do Array o ID que eu estou querendo remover do array.
+}
+
 const buttonAddCart = () => { // função para buscar add os items quando clicado no buttom e incluir na linha.
   const button = document.querySelectorAll('.item__add');
   button.forEach((item) => item.addEventListener('click', async (event) => {
@@ -82,44 +89,48 @@ const buttonAddCart = () => { // função para buscar add os items quando clicad
   }));
 };
 
+const showProductList = async () => {
+  clearProductList();
+  showLoading();
+  const responseRaw = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const responseJson = await responseRaw.json();
+  hideLoading();
+  responseJson.results.forEach((element) => {
+    const itemEl = createProductItemElement(
+      {
+        sku: element.id, name: element.title, image: element.thumbnail,
+      },
+    );
+    document.getElementsByClassName('items')[0].appendChild(itemEl);
+  });
+  buttonAddCart();
+};
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-function cartItemClickListener(event) { // Função que remove o item do carrinho quando clicado
-  // console.log(event.target.id);
-  // filter está retirando do Array o ID que eu estou querendo remover do array.
-  cartList = cartList.filter((item) => item.id !== event.target.id);
-  showCartList();
-}
-
-function showCartTotal() {
-  const total = cartList.reduce((acc, item) => acc + item.price, 0);
-  document.getElementsByClassName('total-price')[0].innerText = total;
-}
-
 const setUpEmptyCart = () => { // função setUpEmptyCart, zera o carrinho toda vez que utilizada.
-  document.getElementsByClassName('empty-cart')[0];
   const buttomEmpty = document.getElementsByClassName('empty-cart')[0]; // buscar o buttum
   buttomEmpty.addEventListener('click', () => { // add eventLister click
     cartList = []; // esvazia o array antes de chamar novamente a lista
     showCartList(); // chama a função que mostra novamente a Lista
   });
 };
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.id = sku; // vai passar esse ID para a função de click para filtrar do array
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-// requisito 1 grupo de estudo com Alvaro Raminelli dev Pleno CoinBase
+// Grupo de estudo com Alvaro Raminelli dev Pleno CoinBase
 window.onload = () => {
+  // function loadCartFromLocalStorage() { 
+  //   localStorage.setItem('item', cartList.innerText);
+  // }
+
+  //   function upDateCartLocalStorage() {
+
+  //     JSON.stringify() setItem()
+  // }
+  document.querySelector('.cart__items').addEventListener('click', cartItemClickListener);
   showProductList();
   showCartList();
-  setUpEmptyCart();
   showCartTotal();
+  setUpEmptyCart();
+  // loadCartFromLocalStorage();
+  // upDateCartLocalStorage();
 };
