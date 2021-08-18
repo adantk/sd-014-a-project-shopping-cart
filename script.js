8e41f9173aac86ba3730b84e3ff02a2d9d1a48bf
ThDevTrader <thiago.so@hotmail.com>
2021-08-18 15:47:42 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const mainSection = document.querySelector('.items');
+  const mainSection = document.querySelector('.items'); // captura a section superior (.items) para adicionar filhos à ela
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,7 +22,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  mainSection.appendChild(section);
+  mainSection.appendChild(section); // adiciona sections criadas como filhos da section superior (.items)
 
   return section;
 }
@@ -33,6 +33,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  const cartOl = document.querySelector('.cart__items'); // capta ol (carrinho) que é pai do conteúdo a ser removido
+  cartOl.removeChild(event.target); // remove filho (li) em que o evento de click acontece (event.target)
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -43,12 +45,12 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const getApiToInsertItemsOnCart = (id) => {
-  const cartOl = document.querySelector('.cart__items');
+const getApiToInsertItemsOnCart = (id) => { // function busca, diretamente da API, dados dos itens que serão inseridos ao carrinho
+  const cartOl = document.querySelector('.cart__items'); // capta a Ol que representa o conteúdo do carrinho para adicionar filhos à ela
   fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((response) => response.json())
     .then((product) => {
-      cartOl.appendChild(createCartItemElement({
+      cartOl.appendChild(createCartItemElement({ // adiciona filhos invocando a function que cria o elemento li que irá para o carrinho
         sku: product.id,
         name: product.title,
         salePrice: product.price,
@@ -56,12 +58,12 @@ const getApiToInsertItemsOnCart = (id) => {
     });
 };
 
-function addButton() {
-  const addBtn = document.querySelectorAll('.item__add');
-  addBtn.forEach((button) => {
+function addButton() { // fuction que habilita o evento de clique aos botões das sections dos produtos
+  const addBtn = document.querySelectorAll('.item__add'); // querySelectorAll retorna uma NodeList (array)
+  addBtn.forEach((button) => { // array de botões será percorrido e, um a um, habilitados os escutadores de eventos
     button.addEventListener('click', (event) => {
-      getApiToInsertItemsOnCart(getSkuFromProductItem(event.target.parentElement));
-    });
+      getApiToInsertItemsOnCart(getSkuFromProductItem(event.target.parentElement)); // invoca a function que capta dados da API tendo 
+    }); // como parâmetro a function que busca o innerText do elemento identificando o Id necessário. Dica valiosíssima da colega Dayane Barbosa
   });
 }
 
