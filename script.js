ea0ae583fc3432d9b1b010b706f22ff77d6d2dbf
Victor Veloso <victorveloso13@gmail.com>
2021-08-19 16:37:58 -0300

    Cria uma listagem de produtos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const computadorURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,20 +26,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//  return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
+// function cartItemClickListener(event) {
   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//  const li = document.createElement('li');
+//  li.className = 'cart__item';
+//  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//  li.addEventListener('click', cartItemClickListener);
+//  return li;
+// }
+
+async function getProducts() {
+  const response = await fetch(computadorURL);
+  const product = await response.json();
+  return product;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { };
+window.onload = async () => { 
+  const productList = await getProducts();
+  console.log(productList.results);
+  productList.results.forEach((product) => {
+    const element = createProductItemElement(product);
+    const items = document.querySelector('.items');
+    items.appendChild(element);
+  });
+};
