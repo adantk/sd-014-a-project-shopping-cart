c9ad7b9e91bdebb45f57f9bb9b3981b8169fb7c3
vanessabastos <vanessarsb83@gmail.com>
2021-08-19 10:30:47 -0300

    corrigindo

--- script.js
+++ script.js
@@ -40,37 +40,4 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-// Requisito #2
-function addCarrinho() {
-  const lista = document.querySelectorAll('.item__add');
-  const buscaOl = document.querySelector('.cart__items');
-  lista.forEach((item) => {
-    item.addEventListener('click', async (event) => {
-      const buscaId = getSkuFromProductItem(event.target.parentElement);
-      const buscaUrl = await fetch(`https://api.mercadolibre.com/items/${buscaId}`);
-      const buscaUrlJson = await buscaUrl.json();
-      buscaOl.appendChild(createCartItemElement({
-        sku: buscaUrlJson.id,
-        name: buscaUrlJson.title,
-        salePrice: buscaUrlJson.price }));
-    });
-  });
-}
-
-// Requisito #1
-function fetchApiProduct() {
-  const itemApi = document.getElementsByClassName('items')[0];
-
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((Response) => Response.json()
-      .then((computadores) => computadores.results.forEach((comput) => {
-        const { id: sku, title: name, thumbnail: image } = comput;
-        const newItens = createProductItemElement({ sku, name, image });
-        itemApi.appendChild(newItens);
-      }))
-      .then(() => addCarrinho()));
-}
-
-window.onload = () => { 
-  fetchApiProduct();
-};
+window.onload = () => { };
