0729ab438ea4007c90a3f91121a1ad4f3c1cfc51
Silas <silas.oliveira8@gmail.com>
2021-08-20 17:00:35 -0300

    requisito 2, finalizado

--- script.js
+++ script.js
@@ -28,30 +28,53 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-
-const createItemsList = async () => {
-  const apiMarket = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const apiJson = await apiMarket.json();
-  console.log(apiJson);
-  apiJson.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
-    createProductItemElement({ sku, name, image });
-  }); 
-};
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
+// função requisito 2
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+// eu fiz 
+const capFetch = async (item) => {
+  const responseMarket = await fetch(`https://api.mercadolibre.com/items/${item}`);
+  const armazenaObjeto = await responseMarket.json();
+  // eu fiz, captura ol
+  const capFatherLi = document.querySelector('.cart__items');
+  const { id, title, price } = armazenaObjeto;
+  capFatherLi.appendChild(createCartItemElement({
+     sku: id,
+     name: title, 
+     salePrice: price, 
+  }));
+};
+
+function capId() {
+  const capButton = document.querySelectorAll('.item__add');
+    capButton.forEach((element) => {
+      element.addEventListener('click', (marcoPolo) => {
+       const capInfoId = marcoPolo.target.parentElement.firstChild.innerText; 
+       capFetch(capInfoId);
+  });
+  });
+  }
+
+const createItemsList = async () => {
+  const apiMarket = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const apiJson = await apiMarket.json();
+  apiJson.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    createProductItemElement({ sku, name, image });
+  }); 
+    capId();
+};
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 window.onload = () => {
