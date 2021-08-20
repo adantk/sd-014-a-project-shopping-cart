2ba12f109d12ccb400f3e06dbfc10bad158fcaaa
DanFilipe2000 <anielilipe90@gmail.com>
2021-08-20 20:57:26 -0300

    Teste

--- script.js
+++ script.js
@@ -42,12 +42,20 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+const somaTotal = (valor) => {
+  const seletor = document.querySelector('.total-price');
+  let valorTotal = 0;
+  valorTotal += valor;
+  seletor.innerText = `Valor total: R$ ${valorTotal}`;
+};
+
 const fetchParaId = async (id) => {
   const resultProduct = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const jsonProduct = await resultProduct.json();
   const obj = { sku: jsonProduct.id, name: jsonProduct.title, salePrice: jsonProduct.price };
   const ol = document.querySelector('.cart__items');
   ol.appendChild(createCartItemElement(obj));
+  somaTotal(obj.salePrice);
 };
 
 const addEvent = () => {
@@ -74,15 +82,15 @@ const fetchProduct = async () => {
   addEvent();
 };
 
-const btnApagaTudo = document.querySelector('.empty-cart');
-btnApagaTudo.addEventListener('click', apagaTudo);
-
-function apagaTudo () {
-  let lista = document.querySelector('.cart__items');
+function apagaTudo() {
+  const lista = document.querySelector('.cart__items');
   while (lista.hasChildNodes()) {  
       lista.removeChild(lista.firstChild);
   }
-};
+}
+
+const btnApagaTudo = document.querySelector('.empty-cart');
+btnApagaTudo.addEventListener('click', apagaTudo);
 
 window.onload = () => {
   fetchProduct();
