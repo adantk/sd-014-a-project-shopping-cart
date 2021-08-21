3af5ec49d9036e30087fc97f23977a1942f63d90
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-08-20 22:27:37 -0300

    requisito 1 resolvido

--- script.js
+++ script.js
@@ -24,24 +24,45 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// Faz parte do requisito 1
+function adicionarProduto(produtos) {
+  const sessionClassItems = document.querySelector('.items');
+  sessionClassItems.innerHTML = '';
+  produtos.forEach((produto) => {
+    const { id: sku, title: name, thumbnail: image } = produto;
+    const produtoMercadoLivre = createProductItemElement({ sku, name, image });
+    sessionClassItems.appendChild(produtoMercadoLivre);
+  });
 }
 
-function cartItemClickListener(/* event */) {
-  // coloque seu código aqui
+// Faz parte do requisito 1
+// Criando uma lista de produtos consultando API do Mercado Livre.
+function criacaoPesquisaProdutos() {
+  const apiMercadoLivre = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(apiMercadoLivre)
+    .then((response) => response.json())
+    .then((ObjetoProduto) => (adicionarProduto(ObjetoProduto.results)))
+    .catch((error) => console.log(error));
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(/* event */) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 window.onload = function onload() {
-  createProductItemElement();
-  getSkuFromProductItem();
-  createCartItemElement();
+  criacaoPesquisaProdutos();
+  // getSkuFromProductItem();
+  // createCartItemElement();
 };
