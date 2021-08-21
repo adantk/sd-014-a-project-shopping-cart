f50538d1db541480813f5ec6f16877e4ee9392c9
Victor hugo Souza <victoraster2014@gmail.com>
2021-08-21 00:43:11 -0300

    requisito 2 concluido

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -46,6 +46,24 @@ async function fetchComputers() {
   document.querySelector('.items').appendChild(createProductItemElement(element));
  });
 }
+async function fetchProduct(id) {
+  const apiResult = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const respToJson = await apiResult.json();
+  return respToJson;
+}
+   const cart = document.querySelector('.cart__items');
+   async function addToCart() {
+   await fetchComputers();
+   const eventAdd = document.querySelectorAll('.item__add');
+   eventAdd.forEach((item) => item.addEventListener('click', async (event) => {
+   const itemID = getSkuFromProductItem(event.target.parentElement);
+   const productInfo = await fetchProduct(itemID);
+   // const productToJson = await productInfo.json();
+   const createdProduct = createCartItemElement(productInfo);
+    cart.appendChild(createdProduct);
+  }));
+}
 window.onload = () => {
   fetchComputers();
- };
+  addToCart();
+};
