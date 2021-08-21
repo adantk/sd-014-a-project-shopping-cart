42b33c06cac4bb764aefdf4bcf392bf7f54a5821
User <gabrielkleber006@gmail.com>
2021-08-21 18:20:24 -0300

    requisitos 3,6,7.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const Ol = document.querySelector('.cart__items');
+const totalPrice = document.querySelector('.total-price');
+const buttonEmpty = document.querySelector('.empty-cart');
+const load = document.querySelector('.loading');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,14 +10,18 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) { 
+function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({
+  sku,
+  name,
+  image,
+}) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -23,7 +32,7 @@ function createProductItemElement({ sku, name, image }) {
 
   const ItensClass = document.querySelector('.items');
   ItensClass.appendChild(section);
-  
+
   return section;
 }
 
@@ -32,49 +41,93 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // coloque seu código aqui  
+  const cartList = document.querySelector('.cart__items');
+  cartList.removeChild(event.target);
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({
+  sku,
+  name,
+  salePrice,
+}) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-const capOl = document.querySelector('.cart__items');
 
 const fetchAPI = async () => {
   const responseComputer = await fetch(
     'https://api.mercadolibre.com/sites/MLB/search?q=computador',
   );
+  load.remove();
   const data = await responseComputer.json();
-    data.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
-    createProductItemElement({ sku, name, image });
+  data.results.forEach(({
+    id: sku,
+    title: name,
+    thumbnail: image,
+  }) => {
+    createProductItemElement({
+      sku,
+      name,
+      image,
+    });
   });
-  };
-
-  const fetchId = async (id) => {
-    try {
-        const responseRaw = await fetch(
-          `https://api.mercadolibre.com/items/${id}`,
-        );
-        const responseJson = await responseRaw.json();
-        const a = { sku: responseJson.id, name: responseJson.title, salePrice: responseJson.price };
-        capOl.appendChild(createCartItemElement(a));
-    } catch (error) {
-        console.log(error);
-    }
 };
 
-const manipEvents = () => {
+const fetchId = async (id) => { // Essa função trata os dados da API e aplica na função de adicionar itens no carrinho
+  try {
+    const responseRaw = await fetch( // estou fazendo a requisição da API, para consumir as informações do produto
+      `https://api.mercadolibre.com/items/${id}`,
+    );
+    const infoP = await responseRaw.json(); // tratando a resposta, e transformando os dados em Json
+    const cartItem = {
+      sku: infoP.id,
+      name: infoP.title,
+      salePrice: infoP.price,
+    }; // desestruturando o objeto recebido para manipular algumas informações
+    Ol.appendChild(createCartItemElement(cartItem)); // adicionando os itens na lista de compra
+    const { price } = infoP;
+    totalPrice.innerText = `${price}`;
+  } catch (error) { // com uma função pré-existente apenas chamando-a como callback e aplicando os valores capturados
+    console.log(error);
+  }
+};
+
+// preciso fazer a soma dos valores dos produtos a medida que eles vão sendo adicionados no carrinho
+
+const manipEvents = () => { // adiciona o evento de clique aos botões e de adicionar ao carrinho
   const searchButton = document.querySelectorAll('.item__add');
   searchButton.forEach((item) => item.addEventListener('click', async (event) => {
     const getSku = getSkuFromProductItem(event.target.parentElement);
     await fetchId(getSku);
-  })); 
+  }));
 };
-window.onload = async () => { 
+
+const apaga = () => {
+  while (Ol.hasChildNodes()) {
+    Ol.removeChild(Ol.firstChild);
+  }
+};
+buttonEmpty.addEventListener('click', apaga);
+// const sumValues = () => {
+// const sumButton = document.querySelectorAll('.item__add');
+// sumButton.addEventListener('click', async (event) => {
+
+// });
+
+// Cada vez que se adicionar um item ao carrinho de compras, será necessário 
+// somar seus valores e apresentá-los na página principal do projeto.  
+// O elemento que tem como filho o preço total dos itens do carrinho deve ter, 
+// obrigatóriamente, a classe total-price.
+
+// Obs: Devemos tomar cuidado, no entanto, pois estamos buscando os dados do produto em uma API.
+//  Portanto, é necessário garantir que a API já retornou as informações para somente depois realizar 
+//  o cálculo da soma.
+
+window.onload = async () => {
   await fetchAPI();
-  await manipEvents();
+  manipEvents();
 };
