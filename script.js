eb0cfa13b0be40710fa8a5634cfaa481ed231fc3
Victor Varges <victorryandias@gmail.com>
2021-08-22 18:41:46 -0300

    Requisito (5) adicionando soma dos valores totais dos produtos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const itemsCart = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -31,7 +33,23 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// const localStorageSave = () => {
+//   localStorage.clear();
+//   const ol = document.querySelector(itemsCart);
+//   localStorage.setItem('listCart', ol.innerHTML);
+// };
+
+// const localStorageLoad = () => {
+//   const ol = document.querySelector(itemsCart);
+//   ol.innerHTML = localStorage.getItem('listCart');
+// localStorageSave();
+// };
+
+// 3. Remova o item do carrinho de compras ao clicar nele
 function cartItemClickListener(event) {
+  //  const ol2 = document.querySelector(itemsCart);
+  //   ol2.removeChild(event.target);
+  // localStorageSave();
   event.target.remove();
 }
 
@@ -43,7 +61,18 @@ function createCartItemElement({ sku, name, salePrice }) {
 
   return li;
 }
-
+// Requisito 4:
+function sum() {
+  const classTotalPrice = document.querySelector('.total-price');
+  let contador = Number(classTotalPrice.innerText);
+  const li = document.querySelectorAll('.cart__item');
+  const num = Number(li[li.length - 1].innerText.split('$')[1]);
+  contador += num;
+  classTotalPrice.innerText = contador;
+  console.log(contador);
+  console.log(sum);
+}
+// 2. Adicione o produto ao carrinho de compras
 const fetchParaId = async (id) => {
   const resultProduct = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const jsonProduct = await resultProduct.json();
@@ -51,6 +80,7 @@ const fetchParaId = async (id) => {
   const obj = { sku: jsonProduct.id, name: jsonProduct.title, salePrice: jsonProduct.price };
   const ol = document.querySelector('.cart__items');
   ol.appendChild(createCartItemElement(obj));
+  sum();
 };
 
 const clickButton = () => {
@@ -62,6 +92,8 @@ const clickButton = () => {
     });
   });
 };
+
+// 1. Crie uma listagem de produtos
 const pegaComputador = async () => {
   const resulComputador = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const jsonComputador = await resulComputador.json();
@@ -73,8 +105,9 @@ const pegaComputador = async () => {
   clickButton();
 };
 
+// 6. Crie um botão para limpar carrinho de compras
 function apaga() {
-  const itensCart = document.querySelector('.cart__items');
+  const itensCart = document.querySelector(itemsCart);
   while (itensCart.hasChildNodes()) {
     itensCart.removeChild(itensCart.firstChild);
   }
@@ -84,3 +117,4 @@ botaoEsvazia.addEventListener('click', apaga);
 // seleciono o item do meu carrinho, que me retorna uma espécie de array. 78, utilizo o while que 'enquanto' o itensCart tiver filhos, eu quero que remova o primeiro filho. HasChildNodes não leva parametro, me retornando true ou false. RemoveChild, recebe um parametro, na qual um elemento que ele tem que receber.  
 
 window.onload = () => { pegaComputador(); };
+// localStorageLoad();
