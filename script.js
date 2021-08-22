7fb78ed1b4c57fd4dc16dd859cda327171620ba1
César Meira <cesarmeirasouza@gmail.com>
2021-08-22 14:32:34 -0300

    Req 2 add item to cart

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const itemContainer = document.querySelector(".items");
+const shoppingCart = document.querySelector('.cart__title');
 
 const keyWord = "computador";
 const url = `https://api.mercadolibre.com/sites/MLB/search?q=${keyWord}`;
@@ -39,12 +40,13 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  console.log(event);
 }
 
 function createCartItemElement({
-  sku,
-  name,
-  salePrice
+  id: sku,
+  title: name,
+  price: salePrice
 }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -53,12 +55,25 @@ function createCartItemElement({
   return li;
 }
 
+async function addItemToCart(event) {
+  //const buttonElement = event.target;
+  const itemID = getSkuFromProductItem(event.target.parentNode);
+  const itemObj =  await (await fetch(`https://api.mercadolibre.com/items/${itemID}`)).json();
+  const cartList = document.querySelector(".cart__items");
+  cartList.appendChild(createCartItemElement(itemObj));
+}
+
+function addClickEventToItemList() {
+  const listButtons = document.querySelectorAll(".item__add");
+  listButtons.forEach((button) => button.addEventListener('click', addItemToCart));
+}
+
 async function search() {
   const response = await (await fetch(url)).json();
   response.results.forEach(element => {
-    console.log(element.id, element.title, element.thumbnail);
     itemContainer.appendChild(createProductItemElement(element));
   });
+  addClickEventToItemList();
 }
 
 window.onload = () => {
