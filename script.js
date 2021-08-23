5d11718528054ace90b9e1a70359cf6c6e752c43
Victor hugo Souza <victoraster2014@gmail.com>
2021-08-23 00:41:31 -0300

    correção do requisito1 e adição de 3, e emptycart

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// por necessidade de reutilizar o mesmo caminho, e evitar repetição
+const currentCart = document.querySelector('.cart__items');
+const itemsFromSection = document.querySelector('.items');
+const saveToLocalStorage = () => {
+  localStorage.setItem('savedCart', currentCart.innerHTML);
+};
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -29,6 +35,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.currentTarget.remove();
+  saveToLocalStorage();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -38,12 +46,18 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+// de alguma forma a função abaixo não passa no teste
+// diz gerar 99 ao inves de 50 items mas não consigo descobrir a razão disso acontecer
 async function fetchComputers() {
   const api = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const resp = await api.json();
   const result = resp.results;
-  result.forEach((element) => {
-  document.querySelector('.items').appendChild(createProductItemElement(element));
+  let counter = 0;
+  result.every((element) => {
+    if (counter >= 50) return false;
+    counter += 1;
+    console.log(counter);
+   return document.querySelector('.items').appendChild(createProductItemElement(element));
  });
 }
 async function fetchProduct(id) {
@@ -56,14 +70,33 @@ async function fetchProduct(id) {
    await fetchComputers();
    const eventAdd = document.querySelectorAll('.item__add');
    eventAdd.forEach((item) => item.addEventListener('click', async (event) => {
+     // ótima dica abaixo da Jéssica Grunewald me ajudou bastante em como selecionar o id 
+     // bem como do Márcio luiz em como targetar a lista do carrinho
    const itemID = getSkuFromProductItem(event.target.parentElement);
+   // agradecimentos ao Gabriel por me explicar como fazer um comportamento assincrono 
    const productInfo = await fetchProduct(itemID);
    // const productToJson = await productInfo.json();
    const createdProduct = createCartItemElement(productInfo);
     cart.appendChild(createdProduct);
   }));
+  saveToLocalStorage();
 }
-window.onload = () => {
-  fetchComputers();
-  addToCart();
+const getFromLocalStorage = () => {
+  itemsFromSection.innerHTML = localStorage.getItem('savedCart');
+  itemsFromSection.childNodes.forEach((li) => li.addEventListener('click', 
+  cartItemClickListener));
+};
+const emptyCart = () => {
+  const cleatButton = document.querySelector('.empty-cart');
+  cleatButton.addEventListener('click', () => {
+  currentCart.innerHTML = '';
+  localStorage.clear();
+  saveToLocalStorage(); 
+  });
+};
+window.onload = async () => {
+  await fetchComputers();
+  getFromLocalStorage();
+  await addToCart();
+  emptyCart();
 };
