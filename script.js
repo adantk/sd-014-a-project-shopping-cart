dfeee24e46150c9b3d408e9114ea77ee1f8a3985
Danilo Menezes <danilomenezes66@gmail.com>
2021-08-24 15:05:08 -0300

    Requisito 2 - Add product to shopping cart

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function createProductItemElement({ sku, name, image }) {
   const sectionItems = document.querySelector('.items');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__ sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
@@ -35,17 +35,36 @@ function cartItemClickListener(event) {
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
+  const ol = document.querySelector('.cart__items');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  ol.appendChild(li);
   return li;
 }
 
+// Requisito 2 - p1
+const addCart = async (productId) => {
+  const productSearch = await fetch((`https://api.mercadolibre.com/items/${productId}`));
+  const pSJson = await productSearch.json();
+  createCartItemElement({ sku: pSJson.id, name: pSJson.title, salePrice: pSJson.price });
+};
+
+// Requisito 2 - p2
+const addCartBtn = () => {
+  const btnAddToCart = document.querySelectorAll('.item__add');
+  btnAddToCart.forEach((btn) => btn.addEventListener('click', (event) => {
+    addCart(event.target.parentElement.firstChild.innerText);
+  }));
+};
+
+// Requisito 1
 const getAPIProduct = async () => {
-  const getAPI = await fetch(('https://api.mercadolibre.com/sites/MLB/search?q=computador')); // Criei uma constante que recebe a API
-  const convertAPIJson = await getAPI.json(); // Criei outra constante que faz a conversão da API para .json
+  const getAPI = await fetch(('https://api.mercadolibre.com/sites/MLB/search?q=computador'));
+  const convertAPIJson = await getAPI.json();
   convertAPIJson.results.forEach(({ id: sku, title: name, thumbnail: image }) =>
-    createProductItemElement({ sku, name, image })); // Faço a busca das informações do array em results e passo como parametro um objeto com id, nome e imagem para a criação do produto
+    createProductItemElement({ sku, name, image }));
+    addCartBtn();
 };
 
 window.onload = () => {
