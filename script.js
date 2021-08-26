cf7791cf9c3f6a7fbfd11fd0d23c5b602dad57af
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-08-26 01:13:28 -0300

    Requisito 01

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
+  const e = document.createElemeent(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
+// Requsito 01 - A função abaixo também é chamada na função mercadoFetchApi().
 function createProductItemElement({ sku, name, image }) {
+  const upSection = document.querySelector('.items'); // cria uma constante que retorna o elemento section de classe 'items' (localizada acima da section de classe cart) para inserir elementos filhos para ela.
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -20,6 +22,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  upSection.appendChild(section); // Insere elementos(sections) filhos de upSection(section criada).
 
   return section;
 }
@@ -40,4 +43,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// Início
+
+// Requisito 01 - 
+function mercadoFetchApi(item) {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`)
+    .then((r) => r.json()
+      .then((products) => products.results.forEach((product) => {
+        createProductItemElement({ 
+          sku: product.id,
+          name: product.title, 
+          image: product.thumbnail,
+        });
+})));
+}
+ 
+window.onload = async () => {
+  mercadoFetchApi('computador'); // é possível buscar outro produto da API, passando outro parâmetro nesta função.
+ };
