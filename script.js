99f4501ec6c5f9849bf163869bc0a640508c6b38
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-08-26 16:30:53 -0300

    Requisitos 01 a 03

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Declaração de Constantes que serão utilizadas
+const olCartItems = '.cart__items';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -11,53 +14,96 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-// Requsito 01 - relacionada à função...
-function createProductItemElement({ sku, name, image }) {
-  const upSection = document.querySelector('.items'); // cria uma constante que retorna o elemento section de classe 'items' (a que fica acima da section de classe cart) para inserir filhos para ela.
-  const section = document.createElement('section');
-  section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+// Requisito 01
 
-  upSection.appendChild(section); // Insere elementos(sections) filhos de upSection(section criada).
+function apiML(mercadoria) {
+  const api = `https://api.mercadolibre.com/sites/MLB/search?q=${mercadoria}`;
+  return new Promise((resolve) => {
+  fetch(api).then((response) => {
+  response.json().then((dado) => {
+    resolve(dado.results);
+    });
+  });
+  });
+}
 
-  return section;
+// Requisito 02
+
+function addToCartAPI(id) { // requisição para o endpoint
+  const endpoint = `https://api.mercadolibre.com/items/${id}`; // valor id do item selecionado.
+  return new Promise((resolve) => {
+    fetch(endpoint).then((response) => {
+      response.json().then((item) => {
+        resolve(item);
+      });
+    });
+  });
+}
+
+function cartItemClickListener(event) { 
+  event.target.remove(); // Ref. site do MDN
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerHTML = `SKU: ${sku} | NAME: ${name} | PRICE: $<span class="price">${salePrice}</span>`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function addToCart() {
+  const addBtn = document.querySelectorAll('.item__add');
+  addBtn.forEach((button) =>
+    button.addEventListener('click', async (event) => {
+      const evento = event.target;
+      const codSku = getSkuFromProductItem(evento.parentElement);
+      const wait = await addToCartAPI(codSku);
+      const cart1 = document.querySelector(olCartItems);
+      cart1.appendChild(createCartItemElement(wait));
+  }));
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// Ref. Requisito 01 - Função para criar os componentes HTML referentes a um produto.
 
-// Início
+const createProductItemElement = async () => {
+  const results = await apiML();
 
-// Requisito 01 - 
-function mercadoFetchApi(item) {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`)
-    .then((r) => r.json()
-      .then((products) => products.results.forEach((product) => {
-        createProductItemElement({ 
-          sku: product.id,
-          name: product.title, 
-          image: product.thumbnail,
-        });
-})));
-}
+  results.forEach(({ id, title, thumbnail }) => {
+    const section = document.createElement('section');
+    section.className = 'item';
+    // abaixo uma constante que retorna o elemento section de classe 'items' (a que fica acima da section de classe cart) para inserir filhos para ela.
+    const mainSection = document.querySelector('.items'); 
+
+    section.appendChild(createCustomElement('span', 'item__sku', id));
+    section.appendChild(createCustomElement('span', 'item__title', title));
+    section.appendChild(createProductImageElement(thumbnail));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+    mainSection.appendChild(section); // Insere elementos(sections) filhos de mainSection(section criada).
+  });
+  addToCart(); // chamada da função 
+};
+
+const clearCart = () => {
+  const btnClear = document.querySelector('.empty-cart');
+  const totalPrice = document.querySelector('.total-price');
+  btnClear.addEventListener('click', () => {
+    const listaDeItens = document.querySelectorAll('.cart__item');
+    listaDeItens.forEach((item) => {
+      item.remove();
+    });
+    totalPrice.innerHTML = '0,00';
+  });
+};
  
 window.onload = async () => {
-  mercadoFetchApi('computador'); // é possível buscar outro produto da API, passando outro parâmetro nesta função.
- };
+  apiML('computador');
+  createProductItemElement();
+  clearCart();
+};
