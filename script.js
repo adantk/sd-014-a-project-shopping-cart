8d6d6f45d2ece9be14ec44a1d43fa6cf37910958
Lucas Sodre <lucasmeinen@yahoo.com.br>
2021-08-27 16:35:39 -0300

    Atualizaçao de alguns requisitos

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+const ol = document.querySelector('.cart__items');
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
+}
+const calculator = {
+  add: (valor, total) => Math.round((valor + total) * 100) / 100,
+  sub: (valor, total) => Math.round((total - valor) * 100) / 100,
+};
+const precoAtualizado = (callback, price) => {
+  let valorInit = 0;
+  const elementosOl = document.querySelectorAll('.cart__item');
+  const totalText = document.querySelector('.total-price');
+  elementosOl.forEach((elemento) => {
+    valorInit = calculator.add(parseFloat(elemento.innerText.split('$')[1]), valorInit);
+  });
+  totalText.innerHTML = callback(price, valorInit);
+};// Ajuda dos colegas!!!
+
+function cartItemClickListener(e) {
+  precoAtualizado(calculator.sub, (parseFloat(e.innerText.split('$')[1])));
+  e.remove();
+  localStorage.setItem('carrinho', ol.innerHTML);
+} // Ajuda dos colegas!!!
+const abastecerCarrinho = () => {
+  ol.innerHTML = localStorage.getItem('carrinho');
+  ol.childNodes.forEach((el) => el.addEventListener('click', (e) =>
+  cartItemClickListener(e.target)));
+  precoAtualizado(calculator.add, 0);
+};// Ajuda dos colegas!!!
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListenner('click', cartItemClickListener);
+  li.addEventListener('click', (e) => cartItemClickListener(e.target));
+  return li;
+}
+const urlApiID = 'https://api.mercadolibre.com/items';
+async function fetchApiID(id) {
+ return fetch(`${urlApiID}${id}`)
+ .then((response) => response.json())
+ .then((dados) => {
+   ol.appendChild(createCartItemElement(dados)); // list add;
+   localStorage.setItem('carrinho', ol.innerHTML); // Salvando no localStorage;
+   precoAtualizado(calculator.add, 0);
+ });
+}
+const eventoButtonId = () => document.querySelectorAll('.item__add').forEach((button) => button
+.addEventListener('click', (e) =>
+fetchApiID(e.target.parentElement.firstChild.innerText)));
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -24,30 +73,17 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  if (event.target.classList.contains('cart__item')) {
-    event.target.remove();
-  }
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+ 
 const findApi = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-const promiseFetch = async (query) => fetch(`${findApi}${query}`)
+const promiseFetchBusca = async (query) => fetch(`${findApi}${query}`)
 .then((response) => response.json()
 .then((dados) => dados.results.forEach((element) =>
-document.querySelector('.items').appendChild(createProductItemElement(element)))));
-
+document.querySelector('.items').appendChild(createProductItemElement(element)))))
+.then(() => eventoButtonId());
 window.onload = () => { 
-  promiseFetch('computador');
+  promiseFetchBusca('computador');
+  abastecerCarrinho();
 };
